62f8b14a79a26ebaf60ca7eeb5073b81b5fc22b2
Joshua Chiang <stevenchiang09@gmail.com>
2025-07-23 17:17:33 -0500

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -11,10 +11,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
+      checks: [],
     }),
     Google({
       clientId: process.env.AUTH_GOOGLE_CLIENT_ID!,
       clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET!,
+      checks: [],
     }),
   ],
   callbacks: {
